84f2d909320ae6dd69b77b04adc370fad1048d97
Dustin Dowell <dustindowell22@gmail.com>
2017-07-25 10:30:13 -0500

    wip code

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const data = (data) => {
+exports.decode = (data) => {
   const result = new Array(data[0].length)
   for (let x = data[0].length; x--;) {
     const item = {}
@@ -10,4 +10,30 @@ const data = (data) => {
   return result
 }
 
-module.exports = data
+// const merge = (a, b) => {
+//   const result = []
+//   const length = a.length > b.length ? a.length : b.length
+//   for (let x = length; x--;) {
+//     result[i] =
+//   }
+// }
+
+exports.encode = (data) => {
+  // const result = new Array(3)
+  // const result = [[], [], []]
+  // const keys = []
+  // const values = []
+  // const model = []
+  for (let x = data.length; x--;) {
+    console.log(
+      Object.keys(data[x])
+    )
+    // for (let y in data[x]) {
+    //   result[0] = y
+    // }
+  }
+}
+
+// exports.keys = (data) => data[0]
+
+// exports.values = (key, data) => data[1][data[0].indexOf(key)]
