ec6966ac0de38bc93dcd72d2c5d51ee56ea04081
Brandon Semilla <semibran@gmail.com>
2017-07-26 12:06:00 -0700

    make it work

---
+++ encode.js
@@ -0,0 +1,26 @@
+module.exports = function encode(items) {
+  var length = items.length
+  var keys = []
+  var values = []
+  var model = new Array(length)
+  for (var i = 0; i < length; i++) {
+    for (var key in items[i]) {
+      var value = items[i][key]
+      var k = keys.indexOf(key)
+      if (k === -1) {
+        k = keys.length
+        keys[k] = key
+        values[k] = [value]
+        model[i][k] = 0
+      } else {
+        var v = values[k].indexOf(value)
+        if (v === -1) {
+          v = values[k].length
+          values[k][v] = value
+        }
+        model[i][k] = v
+      }
+    }
+  }
+  return [keys, values, model]
+}
